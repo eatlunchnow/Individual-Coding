223960839b6e98b0eddcee3aeedaf25b4da3a98d
Kiki Mac <eatlunchnow@users.noreply.github.com>
2017-06-12 10:41:44 -0700

    Add files via upload

---
+++ donation_class.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for donation_class
+/// </summary>
+public class Donations
+{
+    public string currentDonations;
+    public int donationsAmount;
+    public string name;
+    public int totalDonations;
+    public Donations()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public string display()
+    {
+        return "Hello " + name;
+    }
+    public string viewDonations()
+    {
+        return currentDonations;
+    }
+    public int viewDonationAmount()
+    {
+        return donationsAmount;
+    }
+    public int getTotal()
+    {
+        string grantt = "";
+        for (int i = 0; i < donationsAmount; i++)
+        {
+            grantt += "<br>" + donationsAmount;
+        }
+
+        return totalDonations;
+    }
+}
---
+++ grant_class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Login_class
+/// </summary>
+public class Grants
+{
+    public string grantName;
+    public int grantNumber;
+    public string name;
+    public Grants()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public string display()
+    {
+        return "Hello " + name;
+    }
+    public string viewGrants()
+    {
+        return grantName + grantNumber;
+    }
+    
+  }
